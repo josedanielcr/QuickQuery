f8c13edd421487bfc80f9b421b8d14b6850f1ebe
Jose Daniel <jdcanales1905@gmail.com>
2024-01-22 19:08:58 -0600

    Feat: Countries endpoint

--- DataGatewayService/API/Contracts/CountriesResponse.cs
+++ DataGatewayService/API/Contracts/CountriesResponse.cs
@@ -2,5 +2,16 @@
 {
     public class CountriesResponse
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public float CostOfLivingIndex { get; set; } = 0;
+        public float RentIndex { get; set; } = 0;
+        public float CostOfLivingPlusRentIndex { get; set; } = 0;
+        public float GroceriesIndex { get; set; } = 0;
+        public float RestaurantPriceIndex { get; set; } = 0;
+        public float LocalPurchasingPowerIndex { get; set; } = 0;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public int Popularity { get; set; } = 0;
     }
 }
