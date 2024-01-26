fa208ad421e53140aa849303e7dd3a4ebe4f584a
Jose Daniel <jdcanales1905@gmail.com>
2024-01-25 19:04:32 -0600

    Feat: Redis cache connection added

---
+++ SearchService/API/Configuration/AddRedis.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Configuration
+{
+    public static class AddRedis
+    {
+        public static IServiceCollection AddRedisConfiguration(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration =
+                    configuration.GetConnectionString("ConnectionStrings:RedisConnectionString");
+            });
+            return services;
+        }
+    }
+}
---
+++ SearchService/API/Utilities/CacheUtils.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace API.Utilities
+{
+    public class CacheUtils
+    {
+        private readonly IDistributedCache cache;
+
+        public CacheUtils(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+    }
+}
